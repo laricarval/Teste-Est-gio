0416b91d4986d573125e843e707e84fcc88d1fff
Larissa Carvalho <122239002+laricarval@users.noreply.github.com>
2023-02-26 16:36:29 -0300

    Add files via upload

---
+++ questao4.py
@@ -0,0 +1,26 @@
+#QUESTÃO 4
+sp = float(67.83643)
+rj = float(36.67866)
+mg = float(29.22988)
+es = float(27.16548)
+out = float(19.84953)
+
+total = float(sp + rj + mg + es + out)
+print(total)
+
+psp = ((sp/total)*100)
+prj = ((rj/total)*100)
+pmg = ((mg/total)*100)
+pes = ((es/total)*100)
+pout = ((out/total)*100)
+
+print(f'Porcentagem de SP {psp:,.0f}')
+print(f'Porcentagem de RJ {prj:,.0f}')
+print(f'Porcentagem de MG {pmg:,.0f}')
+print(f'Porcentagem de ES {pes:,.0f}')
+print(f'Porcentagem de OUT {pout:,.0f}')
+
+
+
+
+
